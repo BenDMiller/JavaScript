fa016fb1719df09741f68b3e6ac46b43119c52fe
Ben Miller <bbbmiller4@gmail.com>
2024-01-24 20:39:48 -0700

    Update

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+//Get today's date
 date = new Date();
+//Make list of days for later use
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//Get the name of the day of the week
 const currentDayOfWeek = daysOfWeek[date.getDay()];
+//Update all days to be current and the next 4 days
 document.getElementById("one").innerHTML = currentDayOfWeek.substring(0,3);
 document.getElementById("two").innerHTML = daysOfWeek[(daysOfWeek.indexOf(currentDayOfWeek)+1)%7].substring(0,3);
 document.getElementById("three").innerHTML = daysOfWeek[(daysOfWeek.indexOf(currentDayOfWeek)+2)%7].substring(0,3);
